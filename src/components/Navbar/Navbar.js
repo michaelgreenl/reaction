f6b5676c3ea98ea65131a8939eb892fb841276d0
michaelgreenl <lgreener25@gmail.com>
2023-02-24 15:16:01 -0500

    Used NavLink's 'isActive' to make active class and animation for navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,28 +3,33 @@ import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
+  // TODO: Make "Reaction" logo not show up on home page
+  // TODO: Make active NavLink class off if another NavLink is being hovered over
+  // TODO: Add actual :hover animation for activeclass
+  // TODO: Add different :focus animation
+
   return (
     <nav className='navbar'>
       <span className='logo-text'>Reaction</span>
       <ul className='nav-list'>
         <li className='nav-item'>
           <NavLink className='nav-link' to='/'>
-            Home
+            {({ isActive }) => <span className={isActive ? 'nav-link-active' : 'not-active'}>Home</span>}
           </NavLink>
         </li>
         <li className='nav-item'>
           <NavLink className='nav-link' to='/play'>
-            Play
+            {({ isActive }) => <span className={isActive ? 'nav-link-active' : 'not-active'}>Play</span>}
           </NavLink>
         </li>
         <li className='nav-item'>
           <NavLink className='nav-link' to='/improve'>
-            Improve
+            {({ isActive }) => <span className={isActive ? 'nav-link-active' : 'not-active'}>Improve</span>}
           </NavLink>
         </li>
         <li className='nav-item'>
           <NavLink className='nav-link' to='/contact'>
-            Contact
+            {({ isActive }) => <span className={isActive ? 'nav-link-active' : 'not-active'}>Contact</span>}
           </NavLink>
         </li>
       </ul>
