61deb9834da0a9d035a2ab5f5cbb49dd35953933
michaelgreenl <lgreener25@gmail.com>
2023-04-19 19:53:40 -0400

    Added default styles for circle component and added styling prop

--- src/components/Circle/Circle.js
+++ src/components/Circle/Circle.js
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import './Circle.css';
 import { GameSettingsContext } from '../../hooks/GameSettingsContext';
+import { PropTypes } from 'prop-types';
 
-export const Circle = () => {
+export const Circle = (props) => {
   const { gameSettings } = useContext(GameSettingsContext);
 
   const circleStyle = {
+    ...props.styles,
     width: `${gameSettings.circleSize}px`,
     backgroundColor: gameSettings.circleColor,
   };
@@ -15,4 +17,8 @@ export const Circle = () => {
       {gameSettings.showTime && <span className='time'>{gameSettings.shrinkTime}</span>}
     </div>
   );
+};
+
+Circle.propTypes = {
+  styles: PropTypes.object,
 };
