d4ff797393599fd89258df265d3d75c1a426d605
michaelgreenl <lgreener25@gmail.com>
2023-06-06 18:04:04 -0400

    made home screen responsive, along with navbar aside from menu at less than 1024px

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,86 +2,94 @@ import React, { useState } from 'react';
 import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 import { LogoText } from '../Logo/LogoText';
+import { useMediaQuery } from 'react-responsive';
+import { MenuSvg } from '../../svgs/MenuSvg';
 
 export const Navbar = () => {
   const url = window.location.href;
   const [notActiveHovered, setNotActiveHovered] = useState(false);
+  const isSmLaptop = useMediaQuery({ query: '(min-width: 1024px)' });
+  const isTablet = useMediaQuery({ query: '(max-width: 768px)' });
 
   return (
     <nav className='navbar'>
-      <div className='logo-cont' style={url.endsWith('/') ? { visibility: 'hidden' } : undefined}>
-        <LogoText svgSize={{ width: '1.4em' }} textSize={{ fontSize: '1.2em' }} />
+      <div className='logo-cont' style={url.endsWith('/') && isSmLaptop ? { display: 'none' } : undefined}>
+        <LogoText svgClassName='navbar-logo-svg' textClassName='navbar-logo-text' />
       </div>
-      <ul className='nav-list'>
-        <li className='nav-item'>
-          <NavLink className='nav-link' to='/'>
-            {({ isActive }) => (
-              <span
-                className={isActive && !notActiveHovered ? 'nav-link-active' : 'not-active'}
-                onMouseOver={() => {
-                  !isActive ? setNotActiveHovered(true) : setNotActiveHovered(false);
-                }}
-                onMouseOut={() => {
-                  !isActive ? setNotActiveHovered(false) : undefined;
-                }}
-              >
-                Home
-              </span>
-            )}
-          </NavLink>
-        </li>
-        <li className='nav-item'>
-          <NavLink className='nav-link' to='/play'>
-            {({ isActive }) => (
-              <span
-                className={isActive && !notActiveHovered ? 'nav-link-active' : 'not-active'}
-                onMouseOver={() => {
-                  !isActive ? setNotActiveHovered(true) : setNotActiveHovered(false);
-                }}
-                onMouseOut={() => {
-                  !isActive ? setNotActiveHovered(false) : undefined;
-                }}
-              >
-                Play
-              </span>
-            )}
-          </NavLink>
-        </li>
-        <li className='nav-item'>
-          <NavLink className='nav-link' to='/improve'>
-            {({ isActive }) => (
-              <span
-                className={isActive && !notActiveHovered ? 'nav-link-active' : 'not-active'}
-                onMouseOver={() => {
-                  !isActive ? setNotActiveHovered(true) : setNotActiveHovered(false);
-                }}
-                onMouseOut={() => {
-                  !isActive ? setNotActiveHovered(false) : undefined;
-                }}
-              >
-                Improve
-              </span>
-            )}
-          </NavLink>
-        </li>
-        <li className='nav-item'>
-          <NavLink className='nav-link' to='/contact'>
-            {({ isActive }) => (
-              <span
-                className={isActive && !notActiveHovered ? 'nav-link-active' : 'not-active'}
-                onMouseOver={() => {
-                  !isActive ? setNotActiveHovered(true) : setNotActiveHovered(false);
-                }}
-                onMouseOut={() => {
-                  !isActive ? setNotActiveHovered(false) : undefined;
-                }}
-              >
-                Contact
-              </span>
-            )}
-          </NavLink>
-        </li>
-      </ul>
+      {isTablet ? (
+        <MenuSvg className='navbar-menu-icon' />
+      ) : (
+        <ul className={`nav-list ${url.endsWith('/') && isSmLaptop ? 'nav-list-home' : 'nav-list-logo'}`}>
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='/'>
+              {({ isActive }) => (
+                <span
+                  className={isActive && !notActiveHovered ? 'nav-link-active' : 'not-active'}
+                  onMouseOver={() => {
+                    !isActive ? setNotActiveHovered(true) : setNotActiveHovered(false);
+                  }}
+                  onMouseOut={() => {
+                    !isActive ? setNotActiveHovered(false) : undefined;
+                  }}
+                >
+                  Home
+                </span>
+              )}
+            </NavLink>
+          </li>
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='/play'>
+              {({ isActive }) => (
+                <span
+                  className={isActive && !notActiveHovered ? 'nav-link-active' : 'not-active'}
+                  onMouseOver={() => {
+                    !isActive ? setNotActiveHovered(true) : setNotActiveHovered(false);
+                  }}
+                  onMouseOut={() => {
+                    !isActive ? setNotActiveHovered(false) : undefined;
+                  }}
+                >
+                  Play
+                </span>
+              )}
+            </NavLink>
+          </li>
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='/improve'>
+              {({ isActive }) => (
+                <span
+                  className={isActive && !notActiveHovered ? 'nav-link-active' : 'not-active'}
+                  onMouseOver={() => {
+                    !isActive ? setNotActiveHovered(true) : setNotActiveHovered(false);
+                  }}
+                  onMouseOut={() => {
+                    !isActive ? setNotActiveHovered(false) : undefined;
+                  }}
+                >
+                  Improve
+                </span>
+              )}
+            </NavLink>
+          </li>
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='/contact'>
+              {({ isActive }) => (
+                <span
+                  className={isActive && !notActiveHovered ? 'nav-link-active' : 'not-active'}
+                  onMouseOver={() => {
+                    !isActive ? setNotActiveHovered(true) : setNotActiveHovered(false);
+                  }}
+                  onMouseOut={() => {
+                    !isActive ? setNotActiveHovered(false) : undefined;
+                  }}
+                >
+                  Contact
+                </span>
+              )}
+            </NavLink>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -5,35 +5,46 @@ import { Button } from '../../components/Button/Button';
 import { LogoText } from '../../components/Logo/LogoText';
 import { NavLink } from 'react-router-dom';
 import { Footer } from '../../components/Footer/Footer';
+import { useMediaQuery } from 'react-responsive';
 
 function Home() {
+  const isSmLaptop = useMediaQuery({ query: '(min-width: 1024px)' });
+  const isSmPhone = useMediaQuery({ query: '(max-width: 450px)' });
+
   return (
     <div className='home'>
       <Navbar />
       <main className='main'>
-        <section className='logo'>
-          <LogoText svgSize={{ width: '6em' }} textStyles={{ fontSize: '5em', margin: '0' }} />
-          <NavLink to='/play'>
-            <Button text='Play Now' styling={{ fontSize: '1.25em', marginLeft: '15%' }} />
+        {isSmLaptop && (
+          <header className='header'>
+            <LogoText svgClassName='home-logo-svg' textClassName='home-logo-text' />
+          </header>
+        )}
+        <section className='intro'>
+          <h2 className='intro-header'>Why Reaction?</h2>
+          <p className='intro-content'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper pharetra porta. Proin sed velit auctor
+            massa egestas faucibus vel consequat lacus. Consectetur adipiscing elit. Etiam semper pharetra porta. Proin
+            sed velit auctor massa egestas faucib
+          </p>
+          <blockquote className='intro-quote'>
+            &quot;If you ain&apos;t first, you&apos;re last&quot;&nbsp;
+            {isSmPhone && (
+              <>
+                <br />
+                &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+              </>
+            )}
+            - Ricky Bobby
+          </blockquote>
+          <NavLink className='play-now-btn' to='/play'>
+            <Button className='home-button' text='Play Now' />
           </NavLink>
+        </section>
+        <section className='video'>
           <video className='intro-video' loop muted autoPlay>
             <source src='/assets/videos/game.mp4' type='video/mp4' />
           </video>
-        </section>
-        <section className='intro'>
-          <article className='intro-article'>
-            <h2 className='intro-header'>Lorem ipsum dolor sit amet</h2>
-            <p className='intro-content'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper pharetra porta. Proin sed velit
-              auctor massa egestas faucibus vel consequat lacus.
-            </p>
-            <blockquote>Maecenas convallis sit amet ipsum a dapibus. Phasellus</blockquote>
-            <p className='intro-content'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper pharetra porta. Proin sed velit
-              auctor massa egestas faucibus vel consequat lacus. , consectetur adipiscing elit. Etiam semper pharetra
-              porta. Proin sed velit auctor massa egestas faucib
-            </p>
-          </article>
         </section>
       </main>
       <Footer />
