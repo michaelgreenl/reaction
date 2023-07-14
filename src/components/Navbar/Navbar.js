0a25372db8ded15b44ccedb714394b2c3b0f6990
michaelgreenl <lgreener25@gmail.com>
2023-07-13 20:40:12 -0400

    Added NavItem component to Navbar and changed logic

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,78 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback, memo } from 'react';
 import './Navbar.css';
-import { NavLink } from 'react-router-dom';
 import { LogoText } from '../Logo/LogoText';
 import { useMediaQuery } from 'react-responsive';
 import { MenuSvg } from '../../svgs/MenuSvg';
+import NavItem from './NavItem';
 
-export const Navbar = () => {
-  const url = window.location.href;
-  const [notActiveHovered, setNotActiveHovered] = useState(false);
+const Navbar = () => {
+  const url = useRef(window.location.href);
   const isSmLaptop = useMediaQuery({ query: '(min-width: 1024px)' });
   const isTablet = useMediaQuery({ query: '(max-width: 768px)' });
+  const [notActiveHovered, setNotActiveHovered] = useState(false);
+
+  const activeHoverCB = useCallback((toSet) => {
+    setNotActiveHovered(toSet);
+  }, []);
 
   return (
     <nav className='navbar'>
-      <div className='logo-cont' style={url.endsWith('/') && isSmLaptop ? { display: 'none' } : undefined}>
+      <div className='logo-cont' style={url.current.endsWith('/') && isSmLaptop ? { display: 'none' } : undefined}>
         <LogoText svgClassName='navbar-logo-svg' textClassName='navbar-logo-text' />
       </div>
       {isTablet ? (
         <MenuSvg className='navbar-menu-icon' />
       ) : (
-        <ul className={`nav-list ${url.endsWith('/') && isSmLaptop ? 'nav-list-home' : 'nav-list-logo'}`}>
-          <li className='nav-item'>
-            <NavLink className='nav-link' to='/'>
-              {({ isActive }) => (
-                <span
-                  className={isActive && !notActiveHovered ? 'nav-link-active' : 'not-active'}
-                  onMouseOver={() => {
-                    !isActive ? setNotActiveHovered(true) : setNotActiveHovered(false);
-                  }}
-                  onMouseOut={() => {
-                    !isActive ? setNotActiveHovered(false) : undefined;
-                  }}
-                >
-                  Home
-                </span>
-              )}
-            </NavLink>
-          </li>
-          <li className='nav-item'>
-            <NavLink className='nav-link' to='/play'>
-              {({ isActive }) => (
-                <span
-                  className={isActive && !notActiveHovered ? 'nav-link-active' : 'not-active'}
-                  onMouseOver={() => {
-                    !isActive ? setNotActiveHovered(true) : setNotActiveHovered(false);
-                  }}
-                  onMouseOut={() => {
-                    !isActive ? setNotActiveHovered(false) : undefined;
-                  }}
-                >
-                  Play
-                </span>
-              )}
-            </NavLink>
-          </li>
-          <li className='nav-item'>
-            <NavLink className='nav-link' to='/contact'>
-              {({ isActive }) => (
-                <span
-                  className={isActive && !notActiveHovered ? 'nav-link-active' : 'not-active'}
-                  onMouseOver={() => {
-                    !isActive ? setNotActiveHovered(true) : setNotActiveHovered(false);
-                  }}
-                  onMouseOut={() => {
-                    !isActive ? setNotActiveHovered(false) : undefined;
-                  }}
-                >
-                  Contact
-                </span>
-              )}
-            </NavLink>
-          </li>
+        <ul
+          className={`nav-list ${url.current.endsWith('/') && isSmLaptop ? 'nav-list-home' : 'nav-list-logo'} ${
+            notActiveHovered ? 'not-active-hovered' : undefined
+          }`}
+        >
+          <NavItem link='/' text='Home' setNotActiveHovered={activeHoverCB} />
+          <NavItem link='/Play' text='Play' setNotActiveHovered={activeHoverCB} />
+          <NavItem link='/Contact' text='Contact' setNotActiveHovered={activeHoverCB} />
         </ul>
       )}
     </nav>
   );
 };
+
+export default memo(Navbar);
