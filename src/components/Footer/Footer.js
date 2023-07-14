f05d4c5537ebfd5a71744bdb52b7becd73e5a1b1
michaelgreenl <lgreener25@gmail.com>
2023-07-13 20:40:26 -0400

    revert

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,12 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import './Footer.css';
 
-class Footer extends PureComponent {
-  render() {
-    return (
-      <footer className='footer'>
-        <span className='footer-text'>&copy; 2023 Reaction</span>
-      </footer>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <span className='footer-text'>&copy; 2023 Reaction</span>
+    </footer>
+  );
+};
 
 export default Footer;
