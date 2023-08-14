a3851702b01181c97002ed22b28b20d391cd60f8
michaelgreenl <lgreener25@gmail.com>
2023-08-14 17:32:02 -0400

    Updated layout and style for contact page

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from '../../components/Button/Button';
-import { Footer } from '../../components/Footer/Footer';
-import { Navbar } from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+import Navbar from '../../components/Navbar/Navbar';
 import { GithubSvg } from '../../svgs/GithubSvg';
 import './Contact.css';
 
@@ -9,45 +9,48 @@ function Contact() {
   return (
     <div className='contact'>
       <Navbar />
-      <header className='header'>
-        <h1 className='header-text'>Contact</h1>
-      </header>
       <main className='main'>
-        <div className='other-links'>
-          <h2 className='second-header'>Other Links</h2>
-          <button className='other-link'>
-            <GithubSvg className='github-link' />
-          </button>
-        </div>
-        <form className='contact-form' name='contact'>
-          <ul className='form-inputs'>
-            <li className='input-cont'>
-              <label className='input-label' htmlFor='name'>
-                Name
-              </label>
-              <input className='input-field' type='text' name='name' />
-            </li>
-            <li className='input-cont'>
-              <label className='input-label' htmlFor='email'>
-                Email
-              </label>
-              <input className='input-field' type='email' name='email' />
-            </li>
-            <li className='input-cont'>
-              <label className='input-label' htmlFor='subject'>
-                Subject
-              </label>
-              <input className='input-field' type='text' name='subject' />
-            </li>
-            <li className='input-cont'>
-              <label className='input-label' htmlFor='message'>
-                Message
-              </label>
-              <textarea className='input-field input-text-area' name='message'></textarea>
-            </li>
-          </ul>
-          <Button text='Send' styling={{ fontSize: '1.1em', alignSelf: 'flex-end', marginRight: '-5%' }} />
-        </form>
+        <header className='header'>
+          <h1 className='header-text'>Contact</h1>
+          <div className='other-links'>
+            <button className='other-link'>
+              <GithubSvg className='github-link' />
+            </button>
+          </div>
+        </header>
+        <section className='form-cont'>
+          <form className='contact-form' name='contact'>
+            <ul className='form-inputs'>
+              <div className='small-input-cont'>
+                <li className='input-cont'>
+                  <label className='input-label' htmlFor='name'>
+                    Name
+                  </label>
+                  <input className='input-field' type='text' name='name' />
+                </li>
+                <li className='input-cont'>
+                  <label className='input-label' htmlFor='email'>
+                    Email
+                  </label>
+                  <input className='input-field' type='email' name='email' />
+                </li>
+              </div>
+              <li className='input-cont'>
+                <label className='input-label' htmlFor='subject'>
+                  Subject
+                </label>
+                <input className='input-field' type='text' name='subject' />
+              </li>
+              <li className='input-cont'>
+                <label className='input-label' htmlFor='message'>
+                  Message
+                </label>
+                <textarea className='input-field input-text-area' name='message'></textarea>
+              </li>
+            </ul>
+            <Button text='Send' className='submit-button' />
+          </form>
+        </section>
       </main>
       <Footer />
     </div>
