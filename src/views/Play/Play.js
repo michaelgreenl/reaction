da6b0495d56a128b68ec32897c798e05891a7f81
michaelgreenl <lgreener25@gmail.com>
2023-08-17 23:51:47 -0400

    Moved the warning reducer to Play.js

--- src/views/Play/Play.js
+++ src/views/Play/Play.js
@@ -1,7 +1,6 @@
 import React, { useState, useContext, useRef, useCallback, useReducer } from 'react';
 import './Play.css';
 import { UserContext } from '../../hooks/UserContext';
-import { warningReducer } from '../../hooks/warningReducer';
 import Navbar from '../../components/Navbar/Navbar';
 import { Button } from '../../components/Button/Button';
 import GameSettings from '../../components/GameSettings/GameSettings';
@@ -141,5 +140,85 @@ function Play() {
     </div>
   );
 }
+
+const warningReducer = (state, action) => {
+  switch (action.type) {
+    case 'startGameSettingsWarning': {
+      return {
+        ...state,
+        startGameSettingsWarning: {
+          header: 'Before Starting',
+          message:
+            "You haven't saved your new settings yet. Would you like to start the game with the updated settings?",
+          optOutOption: null,
+          buttons: [
+            {
+              text: 'Go Back',
+              className: 'secondary',
+              onClick: () => action.onClick1(),
+            },
+            {
+              text: 'No',
+              className: 'primary',
+              onClick: () => action.onClick2(),
+            },
+            {
+              text: 'Yes',
+              className: 'primary',
+              onClick: () => action.onClick3(),
+            },
+          ],
+        },
+      };
+    }
+    case 'saveGameSettingsWarning': {
+      return {
+        ...state,
+        saveGameSettingsWarning: {
+          header: 'Warning',
+          message: 'Saving these settings will reset your previous scores, since the difficulty will change.',
+          optOutOption: 'saveGameSettingsWarning',
+          buttons: [
+            {
+              text: 'Cancel',
+              className: 'secondary',
+              onClick: () => action.onClick1(),
+            },
+            {
+              text: 'Ok',
+              className: 'primary',
+              onClick: () => action.onClick2(),
+            },
+          ],
+        },
+      };
+    }
+    case 'closeGameSettingsWarning': {
+      return {
+        ...state,
+        closeGameSettingsWarning: {
+          header: 'Warning',
+          message: "You haven't saved your new settings yet. Would you like to save before closing?",
+          optOutOption: 'closeGameSettingsWarning',
+          buttons: [
+            {
+              text: 'No',
+              className: 'secondary',
+              onClick: () => action.onClick1(),
+            },
+            {
+              text: 'Yes',
+              className: 'primary',
+              onClick: () => action.onClick2(),
+            },
+          ],
+        },
+      };
+    }
+    default: {
+      throw Error('Unknown Warning');
+    }
+  }
+};
 
 export default Play;
