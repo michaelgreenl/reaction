99e839a902b0349815e9f1ab681ad96322b38b59
michaelgreenl <lgreener25@gmail.com>
2023-08-18 21:32:45 -0400

    Made Button.js able to take svg and morph svgs

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Button.css';
+import { motion } from 'framer-motion';
 
 export const Button = (props) => {
   return (
     <button
       className={`button ${props.className}`}
       style={{ ...props.styling }}
-      onClick={props.onClick}
+      onClick={() => props.onClick()}
       disabled={props.disabled}
     >
-      <span className='button-text'>{props.text}</span>
+      {props.text && <span className='button-text'>{props.text}</span>}
+      {props.svgInitial && (
+        <motion.svg
+          className={`button-svg ${props.svgClassName}`}
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox={props.viewBox} // Since all view boxes should be the same, (since svgs are morphing between each other).
+          fill='none' // Fill none since stoke is what needs to be animated
+        >
+          <motion.path
+            initial={props.svgInitial}
+            animate={props.svgAnimate}
+            variants={props.svgVariants}
+            transition={props.svgTransition}
+            d={props.svgInitial}
+          />
+        </motion.svg>
+      )}
     </button>
   );
 };
@@ -19,6 +36,12 @@ Button.propTypes = {
   onClick: PropTypes.func,
   className: PropTypes.string,
   styling: PropTypes.object,
-  text: PropTypes.string,
   disabled: PropTypes.bool,
+  text: PropTypes.string,
+  svgClassName: PropTypes.string,
+  viewBox: PropTypes.string,
+  svgInitial: PropTypes.string,
+  svgAnimate: PropTypes.any,
+  svgVariants: PropTypes.object,
+  svgTransition: PropTypes.object,
 };
