2712e54b6b234729c23e40303fe116996ccdbdad
michaelgreenl <lgreener25@gmail.com>
2023-08-18 21:33:33 -0400

    Deleted CloseButton component and replaced it with svg animating Button component

--- src/components/Scores/Scores.js
+++ src/components/Scores/Scores.js
@@ -1,21 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useMemo, useState } from 'react';
 import { UserContext } from '../../hooks/UserContext';
 import './Scores.css';
-import { CloseButton } from '../CloseButton/CloseButton';
+import { Button } from '../Button/Button';
 // import PropTypes from 'prop-types';
 
 export const Scores = () => {
   const { user } = useContext(UserContext);
+  // Close button state and animation states
+  const [closeButton, setCloseButton] = useState('open');
+  const closeButtonVariants = useMemo(() => {
+    return {
+      open: { d: 'M67.5 7.5L7.59623 67.4982M7.5 7.5L67.402 67.5' },
+      closed: { d: 'M67.5 22.5L37.5 52.5M7.5 22.5L37.5481 52.5' },
+    };
+  });
 
   function onCloseClick() {
-    console.log('closing scores');
+    // Setting the close button to the correct svg
+    setCloseButton(closeButton === 'open' ? 'closed' : 'open');
   }
 
   return (
     <div className='scores'>
       <header className='scores-header'>
         <h2 className='scores-header-text'>Scores</h2>
-        <CloseButton animation='down-arrow' onClick={() => onCloseClick()} initialOpenClose='open' />
+        <Button
+          className='close-button'
+          svgClassName='close-button-svg'
+          viewBox='0 0 75 75'
+          svgInitial={closeButtonVariants.open.d}
+          svgAnimate={closeButton}
+          svgVariants={closeButtonVariants}
+          svgTransition={{ duration: 0.25, ease: 'easeInOut' }}
+          onClick={() => onCloseClick()}
+        />
       </header>
       <hr className='scores-break' />
       <ul className='score-list'>
