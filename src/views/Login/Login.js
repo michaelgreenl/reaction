352cb237cb31560c8afb4e5401da030a2acb008b
michaelgreenl <lgreener25@gmail.com>
2023-08-20 17:47:04 -0400

    Added templates for profile and login page

---
+++ src/views/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className='login'>
+      <span>Login</span>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/views/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Profile.css';
+
+function Profile() {
+  return (
+    <div className='profile'>
+      <span>Profile</span>
+    </div>
+  );
+}
+
+export default Profile;
