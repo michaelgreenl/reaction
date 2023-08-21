f3e71522ba27952f73ecc379bb08567a0fb915d0
michaelgreenl <lgreener25@gmail.com>
2023-08-21 17:10:56 -0400

    Changed Scores.js animations to use useAnimation with controls

--- src/components/Scores/Scores.js
+++ src/components/Scores/Scores.js
@@ -1,16 +1,32 @@
-import React, { useContext, useMemo, useState } from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 import { UserContext } from '../../hooks/UserContext';
 import './Scores.css';
 import { Button } from '../Button/Button';
-import { motion } from 'framer-motion';
+import { usePresence, useAnimation, motion } from 'framer-motion';
 // import PropTypes from 'prop-types';
 
 export const Scores = () => {
   const { user } = useContext(UserContext);
+  const [isPresent, safeToRemove] = usePresence();
+  const divControls = useAnimation();
+  const closeBtnControls = useAnimation();
+  const [scoresDiv, setScoresDiv] = useState('closed');
 
-  // Close button state and animation states
-  const [closeButton, setCloseButton] = useState('closed');
-  const closeButtonVariants = useMemo(() => {
+  useEffect(() => {
+    // If the div is open, closed, and if component is getting removed from the dom tree
+    if (isPresent && scoresDiv === 'closed') {
+      divControls.start('closed');
+      closeBtnControls.start('closed');
+    } else if (isPresent && scoresDiv === 'open') {
+      divControls.start('open');
+      closeBtnControls.start('open');
+    } else {
+      divControls.start('exit').then(() => safeToRemove());
+    }
+  }, [isPresent, scoresDiv]);
+
+  // Variants for the scores div motion elements
+  const scoresDivVariants = useMemo(() => {
     return {
       div: {
         open: { maxHeight: '30%', width: '20rem', x: 0 },
@@ -24,18 +40,17 @@ export const Scores = () => {
     };
   });
 
+  // Setting the scores div state to open or closed when the close button is clicked
   function onCloseClick() {
-    // Setting the close button to the correct svg
-    setCloseButton(closeButton === 'open' ? 'closed' : 'open');
+    setScoresDiv(scoresDiv === 'open' ? 'closed' : 'open');
   }
 
   return (
     <motion.div
       className='scores'
-      initial={closeButtonVariants.div.exit}
-      animate={closeButton}
-      variants={closeButtonVariants.div}
-      exit={closeButtonVariants.div.exit}
+      initial={scoresDivVariants.div.exit}
+      animate={divControls}
+      variants={scoresDivVariants.div}
       transition={{ type: 'spring', bounce: 0.25, duration: 0.4 }}
     >
       <header className='scores-header'>
@@ -44,16 +59,16 @@ export const Scores = () => {
           className='close-button'
           svgClassName='close-button-svg'
           viewBox='0 0 75 75'
-          svgInitial={closeButtonVariants.svgs.closed.d}
-          svgAnimate={closeButton}
-          svgVariants={closeButtonVariants.svgs}
+          svgInitial={scoresDivVariants.svgs.closed.d}
+          svgAnimate={closeBtnControls}
+          svgVariants={scoresDivVariants.svgs}
           svgTransition={{ duration: 0.25, ease: 'easeInOut' }}
           onClick={() => onCloseClick()}
         />
       </header>
       <hr className='scores-break' />
       {/* Making the scroll bar thumb transparent during the dropdown closing */}
-      <ul className={`score-list ${closeButton === 'closed' ? 'transparent-scroll' : undefined}`}>
+      <ul className={`score-list ${scoresDiv === 'closed' ? 'transparent-scroll' : undefined}`}>
         {user.scores.map((score, i) => (
           <li key={i} className='score'>
             <span>{score}</span>
